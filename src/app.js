0d307dd0d5a67ba6976dc174d3ea43b0f4328fd6
nuclear.potato <dima.shved.06@gmail.com>
2025-06-25 07:36:07 +0300

    cookie and cors policy

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const config = require('./lib/config');
+const routes = require('./routes/index');
 const app = express();
+
+app.use(cors({
+    origin: '*'
+}));
+app.use(cookieParser(config.cookieSecret));
 
 app.use(express.json());
 app.use(express.urlencoded());
 
-app.get('/ping', (req, res) => {
-    res.send('pong!');
-});
+app.use('/api', routes);
 
 module.exports = app;
--- src/lib/config.js
+++ src/lib/config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    port: process.env.PORT || 8000
+    port: process.env.PORT || 8000,
+    cookieSecret: process.env.COOKIE_SECRET || 'secret'
 }
