07814d4236f2f74bd51b0696b071f01e1c63a660
nuclear.potato <dima.shved.06@gmail.com>
2025-06-28 22:01:13 +0300

    Home page + authProvider fix

--- src/view/src/App.jsx
+++ src/view/src/App.jsx
@@ -1,18 +1,23 @@
 import "./App.css";
 import AuthProvider from "./components/authProvider";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
 import Header from "./layouts/Header";
+import { Home } from "./pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
-      <AuthProvider>
-        <Header />
-        <Routes>
-          <Route index element={<h1>Home page</h1>} />
-          <Route path="*" element={<h1>404</h1>}/>
-        </Routes>
-      </AuthProvider>
+      <Provider store={store}>
+        <AuthProvider>
+          <Header />
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path="*" element={<h1>404</h1>} />
+          </Routes>
+        </AuthProvider>
+      </Provider>
     </BrowserRouter>
   );
 }
--- src/view/src/layouts/Header.jsx
+++ src/view/src/layouts/Header.jsx
@@ -2,7 +2,7 @@ import ProfileMenu from "../components/ProfileMenu";
 
 export default function Header() {
   return (
-    <header className="fixed top-0 left-0 right-0 z-50 backdrop-blur-md bg-white/70 border-b border-gray-200">
+    <header className="fixed top-0 left-0 right-0 z-50 backdrop-blur-md bg-white/50 border-b border-gray-200">
       <div className="container mx-auto px-4 py-3 flex items-center justify-between">
         {/* Левая часть: логотип и навигация */}
         <div className="flex items-center space-x-6">
