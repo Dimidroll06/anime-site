37ea67c3f5146eacadee06c0e3a1549a0251032b
nuclear.potato <dima.shved.06@gmail.com>
2025-07-09 23:17:43 +0300

    file uploader

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,11 @@ const router = express.Router();
 
 const authRoute = require('./auth');
 const userRoutre = require('./user');
+const uploadsRoute = require('./uploads');
 
 router.use('/auth', authRoute);
 router.use('/user', userRoutre);
+router.use('/uploads', uploadsRoute);
 
 router.use('/', (req, res) => {
     res.status(404).json({
