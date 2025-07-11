44707f5c2a838ac13d876c475088d1ceab2fea49
nuclear.potato <dima.shved.06@gmail.com>
2025-07-11 18:48:30 +0300

    basic crud

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,47 @@
+const { User } = require('../models/index');
+const bcrypt = require('bcryptjs');
 
 class UserController {
+
     async me(req, res) {
         return res.status(200).json(req.user);
+    }
+
+    async getUserById(req, res) {
+        const user = await User.findByPk(req.params.id);
+        if (!user) {
+            return res.status(404).send({
+                error: "User not found"
+            });
+        }
+
+        res.status(200).json(user);
+    }
+
+    async editProfile(req, res) {
+        await req.user.update(req.body);
+        return res.status(200).json(req.user);
+    }
+
+    async changePassword(req, res) {
+        const { password } = req.body;
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        req.user.update({
+            hash: hashedPassword
+        });
+
+        res.status(200).json(req.user);
+    }
+
+    async deleteAccount(req, res) {
+        req.user.destroy();
+        res.clearCookie('refreshToken');
+        res.status(204).json({
+            message: 'Account was delleted successfully'
+        });
     }
 }
 
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const userController = require('../controllers/user.controller');
 const { requireAuth } = require('../middleware/auth.middleware');
+const userValidator = require('../validators/user.validator');
 const router = express.Router();
 
+router.get('/:id', userController.getUserById);
 // Чтоб работали cookie
 router.post('/me', requireAuth, userController.me);
+router.put('/', requireAuth, userValidator.validateEditProfile, userController.editProfile);
+router.put('/password', requireAuth, userValidator.validateEditPassword, userController.changePassword);
+router.delete('/', requireAuth, userController.deleteAccount);
 
 module.exports = router;
