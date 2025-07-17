59bd2a5f3a1c2a5e68dad08952936a99da464287
nuclear.potato <dima.shved.06@gmail.com>
2025-07-17 15:21:12 +0300

    Merge pull request #10 from Dimidroll06/feature-anime-route
    anime route

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,10 +4,12 @@ const router = express.Router();
 const authRoute = require('./auth');
 const userRoutre = require('./user');
 const uploadsRoute = require('./uploads');
+const animeRoute = require('./anime');
 
 router.use('/auth', authRoute);
 router.use('/user', userRoutre);
 // router.use('/uploads', uploadsRoute);
+router.use('/anime', animeRoute);
 
 router.use('/', (req, res) => {
     res.status(404).json({
